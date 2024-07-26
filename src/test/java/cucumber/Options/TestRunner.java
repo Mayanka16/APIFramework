fac9ad3e80da2af921f66caa99995e75620a9995
Mayanka Sao <mayankasao@gmail.com>
2024-07-26 20:50:28 +0530

    - Integrated cucumber reporting through https://github.com/damianszczepanik/maven-cucumber-reporting?tab=readme-ov-file added in pom.xml where TestRunner will give the json file where this available code in the above link will convert it into the html report Also remove the classificationFiles tag otherwise the code will fail <classificationFiles>                                     <!-- supports wildcard or name pattern -->                                     <param>sample.properties</param>                                     <param>other.properties</param>                             </classificationFiles>

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,8 +6,11 @@ import io.cucumber.junit.CucumberOptions;
 
 
 // tags = "@DeletePlace")
+
+// maven command to run based on parameters
+// mvn test -D cucumber.filter.tags="@DeletePlace"
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" }, tags = "")
+@CucumberOptions(features = "src/test/java/features", plugin="json:target/jsonReports/cucumber-report.json", glue = { "stepDefinitions" }, tags = "" )
 public class TestRunner {
 
 }
